f6a21cebba8f5aaba14df75cd08d3f241ac6aea8
acse-rc3223 <142494937+acse-rc3223@users.noreply.github.com>
2024-02-02 15:52:36 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,17 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.sep.join((os.curdir, '..'))))
+# Assuming 'docs' is your current working directory,
+# and 'storm_predict/models' is the directory you want to include.
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__name__), '..', 'storm_predict', 'models')))
 
 project = 'Day After Tomorrow'
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon',
-              'sphinx.ext.mathjax',
-              'sphinx_rtd_theme']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
+    'sphinx_rtd_theme'
+]
 html_theme = "sphinx_rtd_theme"
 source_suffix = '.rst'
 master_doc = 'index'
