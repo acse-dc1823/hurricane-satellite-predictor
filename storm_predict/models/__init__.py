d89b8d0a62154e1599f13e5297bc576712d62615
acse-dc1823 <145447968+acse-dc1823@users.noreply.github.com>
2024-02-02 15:58:31 +0000

    Update __init__.py

--- storm_predict/models/__init__.py
+++ storm_predict/models/__init__.py
@@ -1,4 +1,4 @@
-"""Python DL model for generating Hurricane predictions"
+"""Python DL model for generating Hurricane predictions"""
 
 from .image_generate_model import * #noqa
 from .wind_speed_model import * #noqa
